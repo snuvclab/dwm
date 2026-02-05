7a498a4c94013ead149466daa406b4e0c0f7f2f8
Byungjun Kim <peterbj95@gmail.com>
2026-02-05 19:31:38 +0900

    import cfg_skip

--- training/wan_static_pose/utils/__init__.py
+++ training/wan_static_pose/utils/__init__.py
@@ -9,7 +9,7 @@ from .fm_solvers_unipc import FlowUniPCMultistepScheduler
 # from .lora_utils import merge_lora, unmerge_lora
 # from .utils import (filter_kwargs, get_image_latent, get_image_to_video_latent,
 #                     get_video_to_video_latent, save_videos_grid)
-# from .cfg_optimization import cfg_skip
+from .cfg_optimization import cfg_skip
 # from .discrete_sampler import DiscreteSampling
 
 
